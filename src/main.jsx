f7e3d01d31402c2cc4b902f65521999c12d60eb1
lukasbecvar <becvarlukas99@gmail.com>
2025-07-28 17:08:41 +0200

    added particles background

--- src/main.jsx
+++ src/main.jsx
@@ -12,65 +12,73 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import HashGenerator from './components/HashGenerator'
 import PasswordGenerator from './components/PasswordGenerator'
 
+// include other elements
+import ParticlesBackground from './others/ParticlesBackground'
+
 // main app function component routing
 export default function App() {
+    // navigation links
+    const navLinks = [
+        { path: "/", label: "Password generator" },
+        { path: "/hash", label: "Hash generator" }
+    ];
+
     // flag for menu open/close
     const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     return (
         <Router>
-            <div className="flex flex-col min-h-screen bg-[#0c1621]">
+            <div className="relative flex flex-col min-h-screen bg-[#0c1621]">                
+                <ParticlesBackground className="absolute inset-0 z-10"/>
                 {/* navigation bar */}
-                <nav className="bg-[#0c1621] text-white px-6 py-4 border-b border-gray-700 shadow-md">
-                <div className="flex items-center justify-between">
-                    {/* logo */}
-                    <img src="favicon.svg" alt="logo" className="h-7 w-auto mt-[-10px] mb-[-10px]"/>
+                <nav className="bg-[#0c1621ce] text-white px-6 py-4 border-b border-gray-700 shadow-md z-20">
+                    <div className="flex items-center justify-between">
+                        {/* logo */}
+                        <img src="favicon.svg" alt="logo" className="h-7 w-auto mt-[-10px] mb-[-10px]" />
 
-                    {/* mobile menu toggle button */}
-                    <button className="md:hidden text-white focus:outline-none hover:text-blue-400 text-xl mt-[-4px] mb-[-4px]" onClick={() => setIsMenuOpen(!isMenuOpen)}>Menu</button>  
+                        {/* mobile menu toggle button */}
+                        <button className="md:hidden text-white focus:outline-none hover:text-blue-400 text-xl mt-[-4px] mb-[-4px]" onClick={() => setIsMenuOpen(!isMenuOpen)}>Menu</button>
 
-                    {/* desktop menu */}
-                    <div className="hidden md:flex gap-8 text-sm font-semibold tracking-wide uppercase">
-                    <Link to="/" className="group relative text-white">
-                        Password generator
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
-                    <Link to="/hash" className="group relative text-white">
-                        Hash generator
-                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                    </Link>
+                        {/* desktop menu */}
+                        <div className="hidden md:flex gap-8 text-sm font-semibold tracking-wide uppercase">
+                            {navLinks.map(({ path, label }) => (
+                                <Link to={path} key={path} className="group relative text-white">
+                                    {label}
+                                    <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+                                </Link>
+                            ))}
+                        </div>
                     </div>
-                </div>
 
-                {/* mobile menu */}
-                {isMenuOpen && (
-                    <div className="mt-4 flex flex-col gap-4 md:hidden text-sm font-semibold tracking-wide uppercase">
-                        <Link to="/" onClick={() => setIsMenuOpen(false)} className="group relative w-max text-white">
-                            Password generator
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                        </Link>
-                        <Link to="/hash" onClick={() => setIsMenuOpen(false)} className="group relative w-max text-white">
-                            Hash generator
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
-                        </Link>
-                    </div>
-                )}
+                    {/* mobile menu */}
+                    {isMenuOpen && (
+                        <div className="mt-4 flex flex-col gap-4 md:hidden text-sm font-semibold tracking-wide uppercase">
+                            {navLinks.map(({ path, label }) => (
+                                <Link to={path} key={path} onClick={() => setIsMenuOpen(false)} className="group relative w-max text-white">
+                                    {label}
+                                    <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-blue-400 transition-all duration-300 group-hover:w-full"></span>
+                                </Link>
+                            ))}
+                        </div>
+                    )}
                 </nav>
 
                 {/* main component wrapper */}
-                <main className="flex-grow flex flex-col items-center">
-                    <Routes>
-                        <Route path="/" element={<PasswordGenerator/>}/>
-                        <Route path="/hash" element={<HashGenerator/>}/>
-                    </Routes>
+                <main className="relative flex-grow flex flex-col items-center">
+                    <div className="relative w-full h-full text-center">
+                        <Routes>
+                            <Route path="/" element={<PasswordGenerator/>}/>
+                            <Route path="/hash" element={<HashGenerator/>}/>
+                        </Routes>
+                    </div>
                 </main>
 
                 {/* footer */}
-                <footer className="bg-[#0c1621] text-white px-6 py-4 border-t border-gray-700 shadow-md">
+                <footer className="bg-[#0c1621ce] text-white px-6 py-4 border-t border-gray-700 shadow-md z-20">
                     <div className="text-sm font-semibold tracking-wide uppercase text-center">
                         <div className="text-sm font-semibold tracking-wide uppercase text-center">
                             {/* link to author's website */}
-                            <a href="https://becvar.xyz" className="text-blue-400 hover:text-blue-500 mr-1">
+                            <a href="https://becvar.xyz" target="_blank" className="text-blue-400 hover:text-blue-500 mr-1">
                                 Lukáš Bečvář
                             </a>
                             {/* copyright notice */}
